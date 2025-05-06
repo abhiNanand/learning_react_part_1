faac3211953d89f887dd66a72a66c41e7ce28c10
Abhishek Anand <abhishekanand7091@gmail.com>
2025-05-06 23:32:02 +0530

    feat: add customForm using formik and yup

--- app1/src/main.jsx
+++ app1/src/main.jsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Form from './Form/Form.jsx'
-import App2 from './useContext/App2.jsx'
-import App3 from './Rendering_List/App3.jsx';
- 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
 
  
- <App3></App3>
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
   </StrictMode>,
 )
